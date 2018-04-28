34657d08742e066d4d8153ad4cde97fbbde789f0
Nadezhda Hristova <epohster@gmail.com>
2018-04-28 22:50:33 +0300

    feat: Use srcset to load different sizes of images
    - Add alt attributes to images - empty because images are decorations

--- mws-restaurant-stage-1-master/js/main.js
+++ mws-restaurant-stage-1-master/js/main.js
@@ -137,10 +137,14 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
     const li = document.createElement('li');
+    const imgSrc = DBHelper.imageUrlForRestaurant(restaurant);
 
     const image = document.createElement('img');
     image.className = 'restaurant-img';
-    image.src = DBHelper.imageUrlForRestaurant(restaurant);
+    image.alt = "";
+    image.src = imgSrc;
+    image.srcset = `${imgSrc.replace('.jpg', '_small.jpg')} 400w, ${imgSrc} 800w`;
+    image.sizes = "(min-width: 480px) 40vw, (min-width: 721px) 29vw, (min-width: 1000px) 22vw";
     li.append(image);
 
     const name = document.createElement('h1');
@@ -159,9 +163,9 @@ createRestaurantHTML = (restaurant) => {
     const more = document.createElement('a');
     more.innerHTML = 'View Details';
     more.href = DBHelper.urlForRestaurant(restaurant);
-    li.append(more)
+    li.append(more);
 
-    return li
+    return li;
 }
 
 /**
